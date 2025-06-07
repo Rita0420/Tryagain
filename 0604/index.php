48148b8076ad07304ef77e0965435e10805f885f
rita <qazasd1011@gmail.com>
2025-06-07 13:05:27 +0800

    練習

---
+++ 0604/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>美食論壇</title>
+</head>
+<body>
+    <header>
+        <div class="logo"></div>
+        <div class="title">美食論壇</div>
+        <div class="nav">
+            <a href=""></a>
+        </div>
+    </header>
+</body>
+</html>
