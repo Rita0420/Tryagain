ccc6128243396f7ef7785680048dcc99655d245f
rita <qazasd1011@gmail.com>
2025-06-25 15:46:21 +0800

    把ajax的計算改到php

--- 0624/form_ajax/data.php
+++ 0624/form_ajax/data.php
@@ -1,4 +1,41 @@
 <?php
 $data=$_GET;
 
+function dd($data){
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$Avalue=(int)$data['inputA'];
+$Bvalue=(int)$data['inputB'];
+$opt=$data['opt'];
+// echo $Avalue;
+// echo $opt;
+// echo $Bvalue;
+switch ($opt) {
+    case '+':
+        $result = $Avalue + $Bvalue;
+        break;
+
+        case '-':
+        $result = $Avalue - $Bvalue;
+        break;
+
+        case '*':
+        $result = $Avalue * $Bvalue;
+        break;
+
+        case '/':
+        $result = $Avalue / $Bvalue;
+        break;
+
+    default:
+        $result="請輸入+-*/";
+        break;
+}
+
+$data['result']=$result;
+
+
 echo json_encode($data);
